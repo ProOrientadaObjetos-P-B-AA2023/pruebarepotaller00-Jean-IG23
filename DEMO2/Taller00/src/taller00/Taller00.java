05f1994813f74cdc41aa12a3a44c6ca5aca33e98
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-04-12 23:31:25 -0500

    demo2

---
+++ DEMO2/Taller00/src/taller00/Taller00.java
@@ -0,0 +1,10 @@
+package taller00;
+
+public class Taller00 {
+
+    public static void main(String[] args) {
+        System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+              
+    }
+    
+}
